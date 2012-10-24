86697b62498f00c5a4f5e4562f8946ee4e5e0f78
Lauranne Didierjean <lauranne.didierjean@gmail.com>
2012-10-24 16:07:08 +0200

    Updated Parser.js

--- sources/pgli/lang/Parser.js
+++ sources/pgli/lang/Parser.js
@@ -6,70 +6,38 @@ pgli.lang.Parser = gamecore.Base.extend('Parser',
 	TYPE: {
 		NONE: -1,
 		ARITH: 0,
-		NUM: 1
+		NUM: 1,
 		VARIABLE: 2,
 		METHOD: 3
 	},
 
-	patternSpace: /([^ ]+)/ig,
-	patternNum: /[-+]?[0-9]*\.?[0-9]+/ig,
-	patternVar:/^\@[a-z]+/ig,
-	patternArith: /[\+-\--\*-\/]/,
-	patternMethod: /^\#[a-z]+/ig,
+
+	patternVar:/\@([a-z]+)/ig,
+	patternMethod: /\#([a-z]+)/ig,
 
 
 
 	parse: function(string)
 	{
-		var match = this.patternSpace.exec(string);
-		var type = -1;
+		var self = this;
 
-		var numbers = [];
-		var operators = [];
-
-		while(match != null)
+		string = string.replace(this.patternVar,function(match,varName)
 		{
-			match = this.patternSpace.exec(string);
+			return self.getVar(varName);
+		});
 
-			type = this.getTypeOf(match[1]);
 
-			if(type == this.TYPE.VARIABLE)
-				numbers.push(this.getVar(match[1]));
-			if(type == this.TYPE.NUM)
-				numbers.push(match[1]);
-			if(type == this.TYPE.METHOD)
-				numbers.push(this.execFunction(match[1]));
-			if(type == this.TYPE.ARITH )
-				operators.push(match[1]);
+		string = string.replace(this.patternMethod,function(match,methodName)
+		{
+			return self.execFunction(methodName);
+		});
 
-		}
+		console.log(string);
 		
 
+		return eval(string);
+		
 
-	},
-
-	getTypeOf: function(string)
-	{
-		if(this.patternVar.test(string))
-		{
-			return this.TYPE.VARIABLE;
-		}
-		else if(this.patternNum.test(string))
-		{
-			return this.TYPE.NUM;
-		}
-		else if(this.patternArith.test(string))
-		{
-			return this.TYPE.ARITH;
-		}
-		else if(this.patternMethod.test(string))
-		{
-			return this.TYPE.METHOD;
-		}
-		else
-		{
-			return this.TYPE.NONE;
-		}
 	},
 
 	getVar: function(varName)
@@ -81,28 +49,9 @@ pgli.lang.Parser = gamecore.Base.extend('Parser',
 	{
 		return 2;
 	}
-
-
 },
 {
 
 });
 
 
-
-
-/*pgli.lang.ExpressionNode = gamecore.Pooled('ExpressionNode',
-{
-	TYPE: {
-		NONE: -1,
-		ARITH: 0,
-		NUM: 1
-		VARIABLE: 2
-	}
-},
-{
-	value: null,
-	type: -1,
-	children: []
-
-});*/
